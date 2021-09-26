51c876654c1dd87621e653513002697c8be079f6
adhinugroho16 <91260025+adhinugroho16@users.noreply.github.com>
2021-09-26 19:18:09 +0700

    Laporan_Praktikum_Modul1

---
+++ Laporan_Praktikum_Modul1_Menghitung_Luas_Ruangan.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 13:20:30 2021
+
+@author: Surgery Adhi Nugroho
+NIM: 065002100015
+Tugas modul 1 laporan Praktikum
+"""
+
+print("Menghitung panjang dan lebar dari luas sebuah ruangan Persegi panjang")
+print("Hai")
+w = input("Nama kamu siapa?")
+p = float(input("Berapa ukuran panjangnya?"))
+l = float(input("Berapa ukuran lebarnya?"))
+x = input("Berapa ukuran satuan,Meter/inci?") 
+
+L = p*l 
+print("luas sebuah ruangan persegi panjang adalah    = ",L,x)
+
+print("Sekian Terima Kasih")
---
+++ Laporan_Praktikum_Modul1_progam yang menjelaskan tempat tinggal.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 13:32:33 2021
+
+@author: Surgery Adhi Nugroho
+@NIM:065002100015
+"""
+
+print('Hai')
+print('Salam kenal')
+
+print('Nama saya Surgery Adhi Nugroho')
+print('Panggil aja Gery')
+print('Asal dari Jakarta')
+
+
+
+x = input("Nama kamu siapa?")
+print("hallo"),print(x)
+y = input("Asal darimana?")
+print(y)
+z = input("Asal sekolah darimana?")
+print(z)
+a = input("Hobi kamu apa?")
+print(a)
+print("Oke salam kenal"),print(x), print("senang bertemu dengamu")
